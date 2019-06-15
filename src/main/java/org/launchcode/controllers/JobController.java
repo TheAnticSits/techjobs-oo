616ed1e4e2295082dcb29bdbd76d406384b19bb5
Ken Messmer <kenmessmerjr@gmail.com>
2019-06-15 16:58:49 -0500

    Still working on number six.  I feel like i can access the data but adding it to a new job has become problematic.

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,6 +1,7 @@
 package org.launchcode.controllers;
 
 import org.launchcode.models.Job;
+import org.launchcode.models.data.JobDataImporter;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 /**
  * Created by LaunchCode
@@ -42,8 +44,28 @@ public class JobController {
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
+        /*Once you have that stuff in place, you'll be ready to process the form in task #6. Validate the form in the add handler of JobController, and if it's valid, create a new Job object and add it to the data layer by calling jobData.add(newJob).
 
-        return "";
+                To create the new job, you'll need to find the pre-existing objects for all fields other than name (employer, location, etc). Do this using the methods discussed above. Refer to the constructor in Job to make sure you list the objects in the correct order when calling it.
+
+        Once you've created the new job, redirect to the single job display page that you created above. If the jobForm model object fails validation, display the form again.
+        */
+
+        Job newJob = new Job();
+
+
+
+
+        model.addAttribute(jobForm.getName());
+        model.addAttribute(jobForm.getEmployers());
+        model.addAttribute(jobForm.getLocations());
+        model.addAttribute(jobForm.getPositionTypes());
+        model.addAttribute(jobForm.getCoreCompetencies());
+
+
+        jobData.add(newJob);
+
+        return "job-detail";
 
     }
 }
